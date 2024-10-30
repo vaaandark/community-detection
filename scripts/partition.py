8f01c2e9af51591c0a131e352beb77de3c1eac98
vaaandark <vaaandark@outlook.com>
2024-10-30 14:53:42 +0800

    Update visualization

--- scripts/partition.py
+++ scripts/partition.py
@@ -20,12 +20,23 @@ q = nx.community.modularity(G, nx.community.label_propagation_communities(G))
 print(f"graph: communities={ncommunities}, modularity={q}")
 
 if "DRAW" in os.environ:
+    # Compute positions for the node clusters as if they were themselves nodes in a
+    # supergraph using a larger scale factor
+    supergraph = nx.cycle_graph(len(communities))
+    superpos = nx.spring_layout(G, scale=50, seed=429)
+
+    # Use the "supernode" positions as the center of each node cluster
+    centers = list(superpos.values())
+    pos = {}
+    for center, comm in zip(centers, communities):
+        pos.update(nx.spring_layout(nx.subgraph(G, comm), center=center, seed=1430))
+
     cmap = plt.get_cmap("tab20")
     colors = [cmap(i) for i in np.linspace(0, 1, ncommunities)]
-    pos = nx.spring_layout(G)
     for i, nodes in enumerate(communities):
         nx.draw_networkx_nodes(
             G, pos, nodelist=nodes, node_size=20, node_color=colors[i]
         )
     nx.draw_networkx_edges(G, pos, alpha=0.5)
+    plt.tight_layout()
     plt.show()
